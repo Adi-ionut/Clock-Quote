787e922fe893f239f1d7ba6f24462335ae9b3802
Adrian Ionut <ady_ionut91@hotmail.com>
2021-04-16 14:33:17 +0300

    added date

--- app.js
+++ app.js
@@ -3,20 +3,50 @@
 const hourEl = document.querySelector('.hour');
 const minEl = document.querySelector('.min');
 const secEl = document.querySelector('.sec');
+const dateEl = document.querySelector('.date');
 const quoteEl = document.querySelector('.quote');
 const btn = document.querySelector('.btn');
+
+const days = [
+  'Sunday',
+  'Mon',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
 
 function setTime() {
   const date = new Date();
   const seconds = date.getSeconds();
   const minutes = date.getMinutes();
   const hours = date.getHours();
+  const day = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear();
   const secDegrees = (seconds / 60) * 360 + 90;
   const minDegrees = (minutes / 60) * 360 + 90;
   const hourDegrees = (hours / 12) * 360 + 90;
   secEl.style.transform = `rotate(${secDegrees}deg)`;
   minEl.style.transform = `rotate(${minDegrees}deg)`;
   hourEl.style.transform = `rotate(${hourDegrees}deg)`;
+  dateEl.textContent = `${days[day]} / ${months[month]} / ${year}`;
 }
 
 const getQuotes = function () {
@@ -34,7 +64,9 @@ function renderQuotes(data) {
   quoteEl.textContent = `"${quoteText}" -- ${quoteAuthor}`;
 }
 
-getQuotes();
-setInterval(setTime, 1000);
-
 btn.addEventListener('click', getQuotes);
+
+setTime();
+getQuotes();
+setInterval(getQuotes, 300000);
+setInterval(setTime, 1000);
